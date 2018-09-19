2cae3e2fe354df3427ab46dd27e40dcb9b81ebde
Maciej Raczynski <maciej.raczynski@outlook.com>
2018-09-19 12:36:41 +0200

    Initial commit

---
+++ Wi.TenPinBowling/Wi.TenPinBowling.Scoreboard/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Wi.TenPinBowling.Scoreboard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
